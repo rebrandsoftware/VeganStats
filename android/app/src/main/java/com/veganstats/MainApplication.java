fd92f5a3edbaa1bb6ca34c48d849f8e979e2796b
Mike Gibson <mike@rebrandsoftware.com>
2018-10-09 14:59:02 -0400

    Updated for iOS release

--- android/app/src/main/java/com/veganstats/MainApplication.java
+++ android/app/src/main/java/com/veganstats/MainApplication.java
@@ -26,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new RNNotificationsPackage()
+            new RNNotificationsPackage(MainApplication.this)
       );
     }
 
